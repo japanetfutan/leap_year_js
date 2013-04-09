47263b384b62d2188504b80e1fbf6d385db68e6e
japanetfutan <japanetfutan@gmail.com>
2013-04-09 13:56:42 +0900

    Update leap_year.js

--- leap_year.js
+++ leap_year.js
@@ -1,6 +1,14 @@
-//leap year:true,not leap year:false
-var date=new Date();
-var y=date.getFullYear();
-date.setMonth(2);
-date.setDate(0);
-console.log(date.getDate()===28?false:true);
+/**
+ * 
+ * return:
+ * leap year:true,not leap year:false
+ * 
+*/
+function isLeapYear(date){
+  //var date=new Date();
+  var y=date.getFullYear();
+  date.setMonth(2);
+  date.setDate(0);
+  //console.log(date.getDate()===28?false:true);
+  return date.getDate()===28?false:true;
+}
